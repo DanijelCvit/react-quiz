925908ce4125d514c023909c804fe0d952c35703
Danijel C <danijelc+hyf@gmail.com>
2021-11-29 15:32:25 +0100

    Gives error message on non-existing questions and start button

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -12,28 +12,46 @@ export const Question = ({ questions, handleSubmit }) => {
 
   return (
     <div className="question position-relative">
-      <h1 className="mt-3">{`${ID + 1}. ${questions[ID].text}`}</h1>
-      <Answers
-        answers={questions[ID].answers}
-        saveAnswer={saveAnswer}
-        selected={questions[ID].selected}
-      />
-      <div className="buttons-question">
-        <button
-          className="btn btn-primary me-1"
-          onClick={() => {
-            handleSubmit(ID, { selected: localStorage.getItem(ID) });
-          }}
-        >
-          Submit
-        </button>
-        <Link
-          className="btn btn-primary ms-1"
-          to={ID === questions.length - 1 ? "/results" : `/questions/${ID + 1}`}
-        >
-          Next Question
-        </Link>
-      </div>
+      {ID > questions.length - 1 ? (
+        <>
+          <h4>No such question</h4>
+          <div className="center">
+            {" "}
+            <Link className="btn btn-primary" to="/">
+              Go to Start
+            </Link>
+          </div>
+        </>
+      ) : (
+        <>
+          <h1 className="mt-3">{`${ID + 1}. ${questions[ID].text}`}</h1>
+          <Answers
+            answers={questions[ID].answers}
+            saveAnswer={saveAnswer}
+            selected={questions[ID].selected}
+          />
+          <div className="buttons-question">
+            <button
+              className="btn btn-primary me-1"
+              onClick={() => {
+                handleSubmit(ID, { selected: localStorage.getItem(ID) });
+              }}
+            >
+              Submit
+            </button>
+            <Link
+              className="btn btn-primary ms-1"
+              to={
+                ID === questions.length - 1
+                  ? "/results"
+                  : `/questions/${ID + 1}`
+              }
+            >
+              Next Question
+            </Link>
+          </div>
+        </>
+      )}
     </div>
   );
 };
