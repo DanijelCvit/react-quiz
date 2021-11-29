52614af529fd69e2b7f28eeaa9e15d003a76d8a0
Danijel C <danijelc+hyf@gmail.com>
2021-11-29 19:43:26 +0100

    Database selected values cleared automatically from Start

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,17 +1,15 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 export const Start = ({ resetDatabase }) => {
-  localStorage.clear();
-
+  useEffect(() => {
+    resetDatabase();
+  }, []);
   return (
     <div>
       <h1>JAVASCRIPT QUIZ</h1>
       <div className="center mt-4">
-        <Link
-          className="btn btn-primary"
-          onClick={resetDatabase}
-          to={`questions/0`}
-        >
+        <Link className="btn btn-primary" to={`questions/0`}>
           Start Quiz
         </Link>
       </div>
